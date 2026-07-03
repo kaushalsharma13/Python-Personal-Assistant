56203156bbf2571eb6fa38ac0dae85c9e2354116
Kaushal Sharme <kaushalsharma6760576@gmail.com>
2026-07-03 16:59:28 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
 client = OpenAI(
-    api_key="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"
+    api_key="Add your API"
 )
 
